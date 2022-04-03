befbb69fae989631a68292c8a9558666d43dd6b7
Alex Running <runniale000@gmail.com>
2022-04-03 16:13:19 -0500

    updated jscript and html changes to track current date & hour

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,7 @@
 var now = moment().format("MMMM DD, YYYY");
 var time = moment().hours();
-console.log(time);
+var timeBlock = $('.time-block');
+var counter = 9;
 $('#currentDay').text(now);
 
 //save tasks to local storage
@@ -19,3 +20,18 @@ $("#14 .task").val(localStorage.getItem("14"));
 $("#15 .task").val(localStorage.getItem("15"));
 $("#16 .task").val(localStorage.getItem("16"));
 $("#17 .task").val(localStorage.getItem("17"));
+
+for (var i = 0; i < timeBlock.length; i++) {
+    var currentBlock = $('#' + counter);
+    //get id and turn it into an integer
+    var idHour = $('.time-block')[i].id
+    parseInt(idHour);
+    if (idHour < time) {
+        currentBlock.addClass("past");
+    } else if (idHour == time) {
+        currentBlock.addClass("present");
+    } else if (idHour > time) {
+        currentBlock.addClass("future");
+    }
+    counter ++;
+}
